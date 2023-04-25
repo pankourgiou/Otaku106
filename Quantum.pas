7cd504514b6370e4983629a0f6e1cb5cbe9c52d8
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-04-25 06:34:38 +0300

    Update and rename Quantum.pas to Pythagoras9.nb

--- Quantum.pas
+++
@@ -1,2 +0,0 @@
-#There is a thin line in evolve and I love you equation
-#print("E = mc**2, E = mcmod2, Eht = chmod+x, evolve = e*sqrt(82sqrt(e), 128*sqrt(e*980)= I love you, salt, pepper, Kookoorookoo, Rouk Zouk")
